11de2cab60a7a7f90b15dc525a6836b2a0d9cf27
bgete <erictinadogcat@yahoo.com.tw>
2021-12-29 14:19:49 +0800

    init project

---
+++ src/test/java/com/systex/demo/demo6/Demo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systex.demo.demo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
