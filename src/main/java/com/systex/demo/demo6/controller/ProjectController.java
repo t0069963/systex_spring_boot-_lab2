28245eb78cc0f0cf57b8f866ba182f17f2849c75
bgete <erictinadogcat@yahoo.com.tw>
2021-12-29 16:01:56 +0800

    sssss

--- src/main/java/com/systex/demo/demo6/controller/ProjectController.java
+++ src/main/java/com/systex/demo/demo6/controller/ProjectController.java
@@ -5,10 +5,13 @@ import com.systex.demo.demo6.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -16,7 +19,11 @@ public class ProjectController {
     @Autowired
     ProjectService service;
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
+                                              BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+        }
         Project project1 = service.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
--- src/main/java/com/systex/demo/demo6/model/Project.java
+++ src/main/java/com/systex/demo/demo6/model/Project.java
@@ -4,7 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Date;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
@@ -13,18 +15,23 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "project name is required")
     private String projectName;
+    @NotBlank(message = "project Identifier is required")
+    @Size(min=6, max=16, message="idenfier should between 6 to 16")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "description not blank")
     private String description;
     private Date startDate;
     private Date endDate;
     private Date createdAt;
     private Date updatedAt;
-    @PrePersist//EntityManager儲存Entity之前呼叫
+    @PrePersist
     protected void onCreate() {
         this.createdAt = new Date();
     }
-    @PreUpdate//EntityManager將Entity與資料庫同步更新發生前呼叫
+    @PreUpdate
     protected  void onUpdate() {
         this.updatedAt = new Date();
     }
